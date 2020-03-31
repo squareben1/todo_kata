fd93537414f3d175f58abafa2dea5d2bb619d29d
Squareben1 <bengittins@live.com>
2020-03-31 11:25:26 +0100

    add 1

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,5 +1,10 @@
 class Todo
+
   def prompt
     "Type 'Add' + todo to add, type 'Done' + ID to delete"
+  end
+
+  def add(string)
+    ["1. Hello"]
   end 
 end 
--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,7 +1,16 @@
 require 'todo'
 
 describe Todo do 
-  it 'returns string of instructions' do
-    expect(subject.prompt()).to eq "Type 'Add' + todo to add, type 'Done' + ID to delete"
+  describe 'prompt' do
+    it 'returns string of instructions' do
+      expect(subject.prompt()).to eq "Type 'Add' + todo to add, type 'Done' + ID to delete"
+    end
   end 
+
+  describe 'add' do
+    it 'adds new todo item' do
+      expect(subject.add("Hello")).to eq ["1. Hello"]
+    end 
+  end 
+
 end 

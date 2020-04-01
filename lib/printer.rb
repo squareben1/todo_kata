fb996b40a5a18c36aa8a6286639b10a8cdf0bed0
Squareben1 <bengittins@live.com>
2020-04-01 12:35:24 +0100

    prints empty todo list

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,4 +3,8 @@ class Printer
   def print_intro
     'Welcome to the Todo app! Your Todos: '
   end
+
+  def print_todos(arr)
+    "You don't have any todos!"
+  end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,7 +1,16 @@
 require 'printer'
 
 describe Printer do
-  it 'prints intro' do
-    expect(subject.print_intro).to eq 'Welcome to the Todo app! Your Todos: '
+  describe '#print_intro' do
+    it 'prints intro' do
+      expect(subject.print_intro).to eq 'Welcome to the Todo app! Your Todos: '
+    end
+  end
+
+  describe '#print_todos' do
+    it 'prints you have no todos if array empty' do
+      todos = []
+      expect(subject.print_todos(todos)).to eq "You don't have any todos!"
+    end
   end
 end
